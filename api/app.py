8289aac59b93284a8ac9ffa26d135f439dc80525
abraaosantosdev <abraaofilipi12@gmail.com>
2025-08-25 00:54:47 -0300

    Cors policy updated

--- api/app.py
+++ api/app.py
@@ -5,11 +5,15 @@ from api.controllers.taskcontroller import task_bp
 app = Flask(__name__)
 
 # habilita CORS para todas as rotas
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": "*"}})
 
 # middleware para tratar OPTIONS (CORS preflight)
 @app.after_request
 def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    
     if request.method == "OPTIONS":
         response.status_code = 200
     return response
