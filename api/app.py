4460e98d30a3d0eb1734534bfc16a43bb68cada7
abraaosantosdev <abraaofilipi12@gmail.com>
2025-08-25 01:00:35 -0300

    Cors policy updated

--- api/app.py
+++ api/app.py
@@ -5,11 +5,16 @@ from api.controllers.taskcontroller import task_bp
 app = Flask(__name__)
 
 # habilita CORS para todas as rotas
-CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": "*"}})
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5500", "http://localhost:5500", "*"], "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": "*"}})
 
 # middleware para tratar OPTIONS (CORS preflight)
 @app.after_request
 def after_request(response):
+    origin = request.headers.get('Origin')
+    if origin in ["http://127.0.0.1:5500", "http://localhost:5500", "https://seu-frontend.vercel.app"]:
+        response.headers.add('Access-Control-Allow-Origin', origin)
+    
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
