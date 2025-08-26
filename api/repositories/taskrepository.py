0c0bc83f90a89f577788f7f5db952422096bb2a1
abraaosantosdev <abraaofilipi12@gmail.com>
2025-08-26 14:23:14 -0300

    Changed class methods

--- api/repositories/taskrepository.py
+++ api/repositories/taskrepository.py
@@ -6,12 +6,16 @@ def getTasks():
     query = "SELECT * FROM tarefas"
     tarefas = db.executarConsulta(query)
     
-    return [
-        Tarefa(
+    resultado = []
+
+    for tarefa in tarefas:
+        nova_tarefa = Tarefa(
             id=tarefa[0],
             nome_tarefa=tarefa[1],
             status=tarefa[2]
-        ) for tarefa in tarefas
-    ]
-
-
+        )
+        resultado.append(
+            nova_tarefa.to_dict()
+        )
+        
+    return resultado
